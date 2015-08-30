ebc361dce83b3cd7ad9bdbed9081f21188ba2a84
Dhaby Xiloj <dhabyx@gmail.com>
2015-08-30 17:29:33 -0600

    Primera lectura binaria
    Lectura básica por bytes

--- src/lecturabinaria/LecturaBinaria.java
+++ src/lecturabinaria/LecturaBinaria.java
@@ -5,17 +5,59 @@
  */
 package lecturabinaria;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Dhaby Xiloj <dhabyx@gmail.com>
  */
 public class LecturaBinaria {
+    
+    private final String nombreArchivo = "entrada.data";
+    
+    public void lecturaSecuencial(){
+        try {
+            File archivo = new File(nombreArchivo);
+            FileInputStream lectorArchivo = new FileInputStream(archivo);
+            DataInputStream datos;
+            datos = new DataInputStream(lectorArchivo);
+                
+            
+            for (int i=0; i< archivo.length(); i++){
+                byte b;
+                b = datos.readByte();
+                System.out.print(Integer.toHexString(b)+' ');
+            }
+            
+
+//            while (datos.available()>0){
+//                byte b;
+//                b = datos.readByte();
+//                System.out.print(Integer.toHexString(b)+' ');
+//            }
+            
+            datos.close();
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LecturaBinaria.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LecturaBinaria.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LecturaBinaria l = new LecturaBinaria();
+        l.lecturaSecuencial();
     }
     
 }
