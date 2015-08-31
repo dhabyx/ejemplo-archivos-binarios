143393f269b5a78d2a4830eedbd3da44faaae3c5
Dhaby Xiloj <dhabyx@gmail.com>
2015-08-30 18:02:16 -0600

    Agregando prueba de escritura básica

--- src/lecturabinaria/LecturaBinaria.java
+++ src/lecturabinaria/LecturaBinaria.java
@@ -6,9 +6,11 @@
 package lecturabinaria;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,6 +22,7 @@ import java.util.logging.Logger;
 public class LecturaBinaria {
     
     private final String archivoEntrada = "entrada.data";
+    private final String archivoSalida = "salida.data";
     
     public void lecturaSecuencial(){
         try {
@@ -51,13 +54,41 @@ public class LecturaBinaria {
         } 
     }
 
+    public void escrituraSecuencial(){
+        try {
+            File archivo = new File(archivoSalida);
+            FileOutputStream escritorArchivo = new FileOutputStream(archivo);
+            
+            DataOutputStream datos = new DataOutputStream(escritorArchivo);
+            
+            byte [] info = new byte[5];
+            info[0] = 0x48;
+            info[1] = 0x6f;
+            info[2] = 0x6c;
+            info[3] = 0x61;
+            info[4] = 0x0A;
+            
+            datos.write(info);
+            datos.write(info);
+            
+            datos.close();
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LecturaBinaria.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LecturaBinaria.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         LecturaBinaria l = new LecturaBinaria();
-        l.lecturaSecuencial();
+        l.escrituraSecuencial();
     }
     
 }
